3a7ce54cec6ff529c0ff56e224828ee5d86efc90
JoaquinOlmedo <jo608411@gmail.com>
2024-08-28 18:06:32 -0300

    Agregado de pestaña Extras y cambio de color de la pagina

---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    // Mostrar el modal automáticamente después de 2 segundos
+    setTimeout(function(){
+        $('#suscripcionModal').modal('show');
+    }, 2000);
+
+    // Lógica para el botón de suscripción
+    $('#enviar').click(function(){
+        let email = $('#email').val();
+        if (email) {
+            alert('Gracias por suscribirte con el correo: ' + email);
+            $('#suscripcionModal').modal('hide');
+            // Aquí podrías agregar lógica para enviar el correo al servidor
+        } else {
+            alert('Por favor, ingresa un correo electrónico válido.');
+        }
+    });
+});
